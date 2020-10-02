4fe3605aed1c1aac0f24839c52ec8410624b518a
RohanSahana <64703711+RohanSahana@users.noreply.github.com>
2020-10-02 17:57:59 +0530

    Heap Sort

---
+++ Arithmetic/Heap Sort.py
@@ -0,0 +1,32 @@
+# Heap Sort
+import sys 
+a = [ 170, 40, 72, 90, 802, 24, 2, 66, 32]
+
+def heapify(a, n, i): 
+    largest = i 
+    l = 2 * i + 1    
+    r = 2 * i + 2   
+    if l < n and a[i] < a[l]: 
+        largest = l 
+
+    if r < n and a[largest] < a[r]: 
+        largest = r 
+  
+    if largest != i: 
+        a[i],a[largest] = a[largest],a[i]
+        heapify(a, n, largest) 
+  
+def heapSort(a): 
+    n = len(a) 
+    for i in range(n // 2 - 1, -1, -1): 
+        heapify(a, n, i) 
+  
+    for i in range(n-1, 0, -1): 
+        a[i], a[0] = a[0], a[i]  
+        heapify(a, i, 0) 
+
+heapSort(a) 
+n = len(a) 
+print ("\nHeap Sorted array:") 
+for i in range(n): 
+    print ("%d\t" %a[i], sep=' ', end='', flush=True)  
